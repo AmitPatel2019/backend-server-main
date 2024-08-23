5780f16ff9412b3f102fceed25d545f27d6db582
Chandresh Patel <chandreshpatelin.patel@gmail.com>
2024-08-23 00:40:01 -0400

    Update DashboardService.java

--- src/main/java/com/asdsoft/mavala/service/DashboardService.java
+++ src/main/java/com/asdsoft/mavala/service/DashboardService.java
@@ -23,14 +23,14 @@ public class DashboardService {
         );
     }
 
-    public List<DynamicCard> getDynamicCard() {
-        return Arrays.asList(new DynamicCard("1", "Mawala The Board game", "https://starfish-app-trwgh.ondigitalocean.app/Ticket/Mawala - The Board Game.jpg", "Explaination of the board game and various modes to play it", "video",true, true),
-                new DynamicCard("2", "Shiv Shatak - Chronicles of ShivChhatrapati Maharaj", "https://starfish-app-trwgh.ondigitalocean.app/Ticket/100 Stories.jpg", "Brief historical context of 100 events from ShivChhatrapati's life", "audio", true, false),
+       public List<DynamicCard> getDynamicCard() {
+        return Arrays.asList(new DynamicCard("2", "Mawala The Board game", "https://starfish-app-trwgh.ondigitalocean.app/Ticket/Mawala - The Board Game.jpg", "Explaination of the board game and various modes to play it", "video",true, true),
+                new DynamicCard("1", "Shiv Shatak - Chronicles of ShivChhatrapati Maharaj", "https://starfish-app-trwgh.ondigitalocean.app/Ticket/100 Stories.jpg", "Brief historical context of 100 events from ShivChhatrapati's life", "audio", true, false),
                 new DynamicCard("3", "Mawala - Online Curriculum", "https://starfish-app-trwgh.ondigitalocean.app/Ticket/Mawala Workshop.jpg", "Interesting and engaging way to learn unheard historical chapters and animations", "video", true, true),
-                new DynamicCard("4", "Mawala - The Pandora", "https://starfish-app-trwgh.ondigitalocean.app/Ticket/War Stories.jpg", "Aspects of legends from the field about various topics of Shiv Era!", "video", true, false),
+                new DynamicCard("4", "Mawala - The Pandora", "https://starfish-app-trwgh.ondigitalocean.app/Ticket/War Stories.jpg", "Aspects of legends from the field about various topics of Shiv Era!", "video", true, true),
                 new DynamicCard("5", "Battle of Swarajya", "https://starfish-app-trwgh.ondigitalocean.app/Ticket/War Stories.jpg", "Series of Docudramas to learn battle fought by ShivChhatrapati", "video", true, true),
                 new DynamicCard("6", "Shiv Bhushan Strings", "https://starfish-app-trwgh.ondigitalocean.app/Ticket/Shiv Bhushan Strings.jpg", "Elaborations of KaviBhushan's Poems on ShivChhatrapati", "video",true, true),
-                new DynamicCard("7", "स्वराज्याची भूषणे", "https://starfish-app-trwgh.ondigitalocean.app/Ticket/स्वराज्याची भूषणे.jpg", "Information about forts of Sahyadry", "video", true, false),
+                new DynamicCard("7", "स्वराज्याची भूषणे", "https://starfish-app-trwgh.ondigitalocean.app/Ticket/स्वराज्याची भूषणे.jpg", "Information about forts of Sahyadry", "video", true, true),
                 new DynamicCard("8","Swarriors - A Tale of Swarajya Superheroes Battles of Swarajya Battles of Swarajya", "https://starfish-app-trwgh.ondigitalocean.app/Ticket/Swarriors.jpg", "Introduction to Real Indian Superheroes", "video", true, true));
 //    return EMPTY_LIST;
   }
